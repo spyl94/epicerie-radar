f6a0f11fe544f9f4b91c89435a60d581d3f112b7
Aurélien David <spyl94@gmail.com>
2016-12-05 23:22:36 +0100

    Add navbar

---
+++ components/FirstScreen.js
@@ -0,0 +1,41 @@
+/* @flow */
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  Image,
+  View
+} from 'react-native';
+import NavBar from './NavBar';
+
+const logo = require('../android/app/src/main/res/playstore-icon.png');
+
+export default class FirstScreen extends Component {
+
+  render() {
+    return (
+        <View style={styles.loadingScreen}>
+          <Image style={styles.logo} source={logo} />
+          <Text style={{ marginTop: 15 }}>
+            Récupération de votre position...
+          </Text>
+        </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+ container: {
+   ...StyleSheet.absoluteFillObject,
+ },
+ loadingScreen: {
+   flex: 1,
+   flexDirection: 'column',
+   justifyContent: 'center',
+   alignItems: 'center',
+ },
+ logo: {
+   width: 150,
+   height: 150,
+ },
+});
