ee93873aa578e899ac73cf686f69c0b35ea4eda6
Aurélien David <spyl94@gmail.com>
2016-12-13 19:09:10 +0100

    Upgrade react-native

--- android/app/src/main/java/com/epicerie/MainApplication.java
+++ android/app/src/main/java/com/epicerie/MainApplication.java
@@ -4,8 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,8 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativeConfigPackage(),
-            new MapsPackage()
+            new MapsPackage(),
+            new ReactNativeConfigPackage()
       );
     }
   };
