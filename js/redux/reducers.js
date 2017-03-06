0bccc6aa58d86c658fd13f33018d3281214b6e4c
Aurélien David <spyl94@gmail.com>
2017-03-06 23:30:04 +0100

    Clean no more used reducer

--- js/redux/reducers.js
+++ js/redux/reducers.js
@@ -3,11 +3,9 @@ import { combineReducers } from 'redux';
 import location from './modules/location';
 import epicerie from './modules/epicerie';
 import nav from './modules/nav';
-import application from './modules/application';
 
 export default combineReducers({
     nav,
-    application,
     epicerie,
     location,
 });
