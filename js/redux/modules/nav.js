6d6054c1bf93efb802995869e381abbf86e88cd8
Aurélien David <spyl94@gmail.com>
2017-03-13 19:25:53 +0100

    Small enhancements

--- js/redux/modules/nav.js
+++ js/redux/modules/nav.js
@@ -1,4 +1,5 @@
 // @flow
+import { BackAndroid } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import AppNavigator from '../../components/AppNavigator';
 
@@ -13,11 +14,13 @@ export const startShowMapScreenTimer = (dispatch: Function) => {
 
 export default (state: Object, action: Object): Object => {
   if (action.type === 'BACK') {
+    if (state.routes[state.index] && state.routes[state.index].routeName === 'Map') {
+      BackAndroid.exitApp();
+    }
     return AppNavigator.router.getStateForAction(NavigationActions.back(), state);
   }
   if (action.type === 'SHOW_MAP_SCREEN') {
-    const route = state.routes[state.index];
-    if (route && route.routeName === 'Launch') {
+    if (state.routes[state.index] && state.routes[state.index].routeName === 'Launch') {
       const screen = NavigationActions.navigate({ routeName: 'Map' });
       return AppNavigator.router.getStateForAction(screen, state);
     }
