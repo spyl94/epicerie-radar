23f81b9700bbcccd65f2951d658411ba085bca5e
Aurélien David <spyl94@gmail.com>
2017-04-18 20:52:02 +0200

    Fix design

--- js/components/RadioButton.js
+++ js/components/RadioButton.js
@@ -3,6 +3,7 @@ import { SegmentedControls } from 'react-native-radio-buttons';
 
 const RadioButton = ({ input: { onChange, value }, ...otherProps }) => (
   <SegmentedControls
+    tint={'#178c80'}
     {...otherProps}
     onSelection={onChange}
     selectedOption={value}
