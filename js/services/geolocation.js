89d8ea624c05cfe1e6aaf9947a17c1dd2fc40fdf
Aurélien David <spyl94@gmail.com>
2017-09-06 20:33:35 +0200

    Clean

--- js/services/geolocation.js
+++ js/services/geolocation.js
@@ -15,23 +15,6 @@ export const getGoogleAutocompleteUrl = (term: string, position: ?LatLng) => {
   return url;
 };
 
-export const getDistanceBetweenPlaces = (
-  position1: LatLng,
-  position2: LatLng,
-) => {
-  return fetch(
-    `https://maps.google.com/maps/api/directions/json?key=${Config.GOOGLE_MAPS_API_KEY}&origin=${position1.latitude},${position1.longitude}&destination=${position2.latitude},${position2.longitude}&sensor=false&units=metric`
-  )
-    .then(response => response.json())
-    .then(json => {
-      if (json.routes.length > 0) {
-        return json.routes[0].legs[0].duration.value;
-      } else {
-        return false;
-      }
-    });
-};
-
 export const getGeocationData = async (
   startLoc: LatLng,
   destinationLoc: LatLng,
